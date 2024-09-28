edcafd3628765b043f9ee1b720195c477560930f
missipsa ghernaout <ghernaoutmissipsa.pro@gmail.com>
2024-09-28 10:57:34 +0200

    finishing touches

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -11,4 +12,18 @@ const userSchema = new mongoose.Schema({
   }
 })
 
-module.exports = mongoose.model("User" , userSchema)
+//add a static model-method to check if the credentials are valid
+userSchema.statics.findAndValidate = async function (username, password) {
+  const foundUser = await this.findOne({'username' : username});
+  const isUser = await bcrypt.compare(password, foundUser.password);
+  return isUser ? foundUser : null ; 
+}
+
+
+userSchema.pre("save", async function (next) {
+  if (! this.isModified('password')) return next(); 
+  this.password = await  bcrypt.hash(this.password, 12);
+  next();
+})
+
+module.exports = mongoose.model("User" , userSchema);
